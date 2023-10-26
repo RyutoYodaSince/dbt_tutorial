eefd6e22d81d8626c58d1f008fe474f81f6485d0
Ryuto Yoda <yoda.ryuto@since2020.jp>
2023-10-26 03:38:32 +0000

    変えた

--- models/starbucks_drink.sql
+++ models/starbucks_drink.sql
@@ -2,5 +2,5 @@ SELECT
     Beverage_category,
     Beverage_prep,
     Calories
-FROM {{ ref('starbucks_drink.SQL') }}  -- 注意: "starbucks_drink" としています
+FROM {{ ref('starbucks_drink.SQL') }}  
 WHERE Calories >= 350;
